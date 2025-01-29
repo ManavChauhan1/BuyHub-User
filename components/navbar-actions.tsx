2c1228f79747429bbfead9dc7377f69aee5f2e9c
Manav Chauhan <92674597+ManavChauhan1@users.noreply.github.com>
2025-01-29 13:14:08 +0530

    Individual Category Screen(Store) created

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -18,10 +18,9 @@ const NavbarActions = () => {
     }
 
     
-
     return(
         <div className="ml-auto flex items-center gap-x-4">
-            <Button className="flex items-center rounded-full bg-black px-4 py-2">
+            <Button className="flex items-center rounded-full bg-black px-4 py-2" >
                 <ShoppingBag
                     size={20}
                     color="white"
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,39 +3,40 @@ import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
 
 export interface ButtonProps
-    extends React.ButtonHTMLAttributes<HTMLButtonElement>{}
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
-    className,
-    children,
-    disabled,
-    type = "button",
-    ...props
-}, ref) => {
-    return(
-        <button
-            className={cn(
-                `
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
+    return (
+      <button
+        className={cn(
+          `
                 w-auto
                 rounded-full
                 bg-black
                 border-transparent
                 px-5 py-3
-                disabled: cursor-not-allowed
-                disabled: opacity-50
                 text-white
+                disabled:opacity-50
                 font-semibold
-                hover: opacity-75
+                hover:opacity-75
                 transition
+                disabled:cursor-not-allowed
                 `,
-                className
-            )}
-            ref={ref}
-        >
-            {children}
-        </button>
-    )
-})
+          className
+        )}
+        ref={ref}
+        // disabled={disabled}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+// disabled: cursor-not-allowed
+// disabled: opacity-50
 
 Button.displayName = "Button";
 
